0e0cf696bc5ad87dc553deded2fffc1ee6ef5288
eracliogabriel <lopes.biel@hotmail.com>
2024-05-03 02:02:41 -0300

    teste

--- tipos-variaveis/src/Usuario.java
+++ tipos-variaveis/src/Usuario.java
@@ -1,23 +1,30 @@
-
+import java.util.Scanner;
 public class Usuario {
 
 	public static void main(String[] args) {
-			SmartTv smartTv = new SmartTv();
-			
-			smartTv.diminuirVolume();
-			smartTv.diminuirVolume();
-			smartTv.diminuirVolume();
-			smartTv.aumentarVolume();
-			System.out.println("Volume atual: " +smartTv.volume);
-			
-			smartTv.mudarCanal(12);
-			
-			
-			System.out.println("TV Ligada? " +smartTv.ligada);
-			System.out.println("Canal atual: " +smartTv.canal);
-			System.out.println("Volume: " +smartTv.volume);
-			
-			smartTv.ligar();
-			System.out.println("E agora, TV Ligada? " +smartTv.ligada);
+		Scanner scanner = new Scanner(System.in);
+		
+		String agencia, nomeCliente;
+		int numeroConta;
+		double saldo;
+		
+		System.out.println("Por favor, digite o número da Agência:");
+        agencia = scanner.nextLine();
+        
+        System.out.println("Por favor, digite o número da Conta:");
+        numeroConta = scanner.nextInt();
+        
+        scanner.nextLine();
+        
+        System.out.println("Por favor, digite o nome do cliente:");
+        nomeCliente = scanner.nextLine();
+        
+        System.out.println("Por favor, digite o saldo:");
+        saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " +
+                agencia + ", conta " + numeroConta + " e seu saldo de " + saldo + " já está disponível para saque.");
+
+        scanner.close();
 	}
 }
