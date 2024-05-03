4f073e2129e8d80be0a3b55b20dff1c5a9bf0776
eracliogabriel <lopes.biel@hotmail.com>
2024-05-03 02:26:26 -0300

    Criando Projeto e Class projeto Dio trilha java básico

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,30 @@
+	import java.util.Scanner;
+public class ContaTerminal {
+
+		public static void main(String[] args) {
+			Scanner scanner = new Scanner(System.in);
+			
+			String agencia, nomeCliente;
+			int numeroConta;
+			double saldo;
+			
+			System.out.println("Por favor, digite o número da Agência:");
+	        agencia = scanner.nextLine();
+	        
+	        System.out.println("Por favor, digite o número da Conta:");
+	        numeroConta = scanner.nextInt();
+	        
+	        scanner.nextLine();
+	        
+	        System.out.println("Por favor, digite o nome do cliente:");
+	        nomeCliente = scanner.nextLine();
+	        
+	        System.out.println("Por favor, digite o saldo:");
+	        saldo = scanner.nextDouble();
+
+	        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " +
+	                agencia + ", conta " + numeroConta + " e seu saldo de " + saldo + " já está disponível para saque.");
+
+	        scanner.close();
+		}
+	}
